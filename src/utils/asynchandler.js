735a826f0e2dfbd61614a36f87fb3ca02f635928
shukla07052004-eng <shukla07052004@gmail.com>
2026-07-06 01:38:08 +0530

    utils code & middleware

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,21 @@
+// another syntax fpr same functonality
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+// we create a wrapper function that we are going to use purther
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     } catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message || "Internal Server Error"
+//         })
+//     }
+// }
